1846b4f46e0a707206678fee587a7a5298d72f6b
yusrA312 <yusra2yahya@gmail.com>
2023-12-19 04:33:29 -0800

    done

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a class MagicClass"""
+
+import math
+
+
+class MagicClass:
+    """Class that defines properties of MagicClass."""
+
+    def __init__(self, radius=0):
+        """Creates new instances of MagicClass.
+        Args:
+            radius: radius.        
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """Returns area """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Returns circumference."""
+        return 2 * math.pi * self.__radius
+
