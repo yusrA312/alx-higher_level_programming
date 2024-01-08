7f6764faae6c68d65ba6850fd95509b8d34bf9c5
yusrA312 <yusra2yahya@gmail.com>
2024-01-08 08:17:14 -0800

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,32 +12,30 @@ def matrix_divided(matrix, div):
     # Validate matrix
     for row in matrix:
         if not isinstance(row, list) or len(row) == 0:
-            raise TypeError(
-                "matrix must be a matrix (list of lists) " + "of integers/floats"
-            )
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for x in row:
             if not isinstance(x, (int, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) " + "of integers/floats"
-                )
+                raise TypeError("matrix must be a matrix (list of lists) " +
+                                "of integers/floats")
 
     if not isinstance(matrix, list) or len(matrix) == 0:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) " + "of integers/floats"
-        )
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
 
     # Validate div is a number
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     # Perform matrix division and round to 2 decimal places
-    result_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    result_matrix = [
+        [round(element / div, 2) for element in row] for row in matrix
+    ]
     return result_matrix
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testfile("tests/2-matrix_divided.txt")
