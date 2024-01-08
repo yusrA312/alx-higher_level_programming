160ba95b5f8ad2051044299341da8f8bd46c8b11
yusrA312 <yusra2yahya@gmail.com>
2024-01-08 06:57:24 -0800

    done

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""
-Contains the class MyInt
-"""
+"""Module class MyInt"""
 
 
 class MyInt(int):
+    """rebel version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
     def __eq__(self, other):
-        """!= is now =="""
+        """what was != is now =="""
         return int(self) != other
 
     def __ne__(self, other):
-        """== is now !="""
+        """what was == is now !="""
         return int(self) == other
