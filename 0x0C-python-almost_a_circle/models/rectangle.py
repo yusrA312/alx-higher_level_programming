3a54f93d178c94798348c64e7ba47f27e6a5e9bb
yusrA312 <yusra2yahya@gmail.com>
2024-01-12 02:15:04 -0800

    done

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''A Rectangle class inheriting from Base.'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''  init '''
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        ''' The Width.'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.v_inT("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''The Height.'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.v_inT("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''THE x of rectangle.'''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.v_inT("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''THE y of rectangle.'''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.v_inT("y", value)
+        self.__y = value
+
+    def v_inT(self, name, value, eqL=True):
+        '''validating the value.'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eqL and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eqL and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def area(self):
+        '''THE area.'''
+        return self.width * self.height
+
+    def display(self):
+        '''Prints representation oF THE rectangle.'''
+        s = '\n' * self.y + \
+            (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(s, end='')
+
+    def __str__(self):
+        ''' THE info OF rectangle.'''
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width,
+                   self.height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''THE updates instance.'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''THE Updates instance.'''
+
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        '''THE dictionary .'''
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''Module for Square class.'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''A Square class inhrting from Rectangle.'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''init.'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''the Returns string.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        '''the Size.'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __up(self, id=None, size=None, x=None, y=None):
+        '''to updates instance.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''to Updates instance.'''
+        if args:
+            self.__up(*args)
+        elif kwargs:
+            self.__up(**kwargs)
+
+    def to_dictionary(self):
+        '''THE dictionary.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
