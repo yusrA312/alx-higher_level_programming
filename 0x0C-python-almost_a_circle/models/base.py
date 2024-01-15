67c18f66dccca7ee6cc81aa841964a23672a060b
yusrA312 <yusra2yahya@gmail.com>
2024-01-15 09:53:32 -0800

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -133,3 +133,43 @@ class Base:
             t.end_fill()
 
         time.sleep(5)
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw Random shap"""
+        import turtle
+        import time
+        from random import randrange
+
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(4):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(45)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(60)
+            turt.hideturtle()
+
+        turtle.exitonclick()
