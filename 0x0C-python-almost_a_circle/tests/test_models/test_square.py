b607fe6578b65b67f6e43b33cb409bde3f1ccb2c
yusrA312 <yusra2yahya@gmail.com>
2024-01-15 14:41:41 -0800

    done

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,135 +1,133 @@
 #!/usr/bin/python3
-'''Module Rectangle'''
-
-import io
-import sys
+'''ModuleSquare'''
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
+from models.square import Square
+from random import randrange
 from contextlib import redirect_stdout
+import io
 
-class TestRectangle(unittest.TestCase):
-    '''Tests to Base'''
 
-    def test_None_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle(None, 25)
+class TestSquare(unittest.TestCase):
+    '''Tests Base class.'''
 
-    def test_float_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle(5.05, 17)
+    def test_nan_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, float('nan'), 20)
 
-    def test_complex_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle(complex(56), 26)
+    def test_complex_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(7, 36, complex(45))
 
-    def test_dict_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle({"a": 15, "b": 62}, 2)
+    def test_dict_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(16, 10, {"a": 1, "b": 2})
 
-    def test_list_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle([1, 22, 33], 2)
+    def test_list_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(16, 1, [10, 20, 3])
 
-    def test_set_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle({1, 22, 33}, 2)
+    def test_set_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 107, {16, 2, 73})
 
-    def test_tuple_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle((1, 22, 33), 2)
+    def test_tuple_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 1, (8, 2, 93))
 
-    def test_frozenset_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle(frozenset({41, 25, 63, 61}), 2)
+    def test_frozenset_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 3, frozenset({17, 26, 3, 1}))
 
-    def test_range_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle(range(57), 26)
+    def test_range_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 3, range(5))
 
-    def test_bytes_width(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Rectangle(b'P', 32)
-
-    def test_negative_width(self):
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            Rectangle(-1, 26)
-
-    def test_zero_width(self):
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            Rectangle(0, 26)
     def invalid_types(self):
-        m = (3.1, -1.221, float('inf'), float('-inf'), True, "hi", (6,),
-             [7], {5}, {6: 7}, None)
-        return m
+        n = (3.1, -1.51, float('inf'), float('-inf'), True, "hi", (4,),
+             [4], {4}, {9: 77}, None)
+        return n
 
-    def test_J_display_no_args(self):
-        m = Rectangle(9, 8)
+    def test_dict_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, {"a": 4, "b": 5}, 6)
+
+    def test_display(self):
+        m = Square(10)
         with self.assertRaises(TypeError) as e:
-            Rectangle.display()
+            Square.display()
         n = "display() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), n)
 
-    def test_J_display(self):
-        m = Rectangle(1, 1)
-        f = io.StringIO()
-        with redirect_stdout(f):
+    def test_display_s(self):
+        m = Square(1)
+        n = io.StringIO()
+        with redirect_stdout(n):
             m.display()
         p = "#\n"
-        self.assertEqual(f.getvalue(), p)
-        m.width = 2
-        m.height = 6
-        f = io.StringIO()
-        with redirect_stdout(f):
+        self.assertEqual(n.getvalue(), p)
+
+        m.size = 2
+        n = io.StringIO()
+        with redirect_stdout(n):
             m.display()
         p = "\
 ##\n\
 ##\n\
-##\n\
-##\n\
-##\n\
-##\n\
 "
+        self.assertEqual(n.getvalue(), p)
+
+        m = Square(5, 5, 5)
+        f = io.StringIO()
+        with redirect_stdout(f):
+            m.display()
+        p = """\
+
+
+
+
+
+     #####
+     #####
+     #####
+     #####
+     #####
+"""
         self.assertEqual(f.getvalue(), p)
 
-    def test_instan(self):
-        m = Rectangle(1, 30)
-        self.assertEqual(str(type(m)), "<class 'models.rectangle.Rectangle'>")
-        self.assertTrue(isinstance(m, Base))
-        n = {'_Rectangle__height': 30, '_Rectangle__width': 1,
-             '_Rectangle__x': 0, '_Rectangle__y': 0, 'id': 24}
-        self.assertDictEqual(m.__dict__, n)
+        m = Square(8, 8)
+        f = io.StringIO()
+        with redirect_stdout(f):
+            m.display()
+        n = """\
+        ########
+        ########
+        ########
+        ########
+        ########
+        ########
+        ########
+        ########
+"""
+        self.assertEqual(f.getvalue(), n)
 
+    def test_B_inheritance(self):
+        self.assertTrue(issubclass(Square, Base))
+
+    def test_list_size(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square([8, 7, 6])
+
+    def test_set_size(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square({7, 2, 7}, 9)
+
+    def test_J_display_no_args(self):
+        m = Square(3)
         with self.assertRaises(TypeError) as e:
-            m = Rectangle("11", 2)
-        msg = "width must be an integer"
-        self.assertEqual(str(e.exception), msg)
-
-        with self.assertRaises(TypeError) as e:
-            m = Rectangle(11, "2")
-        msg = "height must be an integer"
-        self.assertEqual(str(e.exception), msg)
-
-        with self.assertRaises(TypeError) as e:
-            r = Rectangle(11, 2, "43")
-        msg = "x must be an integer"
-        self.assertEqual(str(e.exception), msg)
-
-    def test_to_dictionary_out(self):
-        m = Rectangle(101, 22, 11, 89, 5)
-        c = {'x': 11, 'y': 89, 'id': 5, 'height': 22, 'width': 101}
-        self.assertDictEqual(c, m.to_dictionary())
-
-    def test_to_dictionary_no(self):
-        m1 = Rectangle(1, 3, 11, 8, 6)
-        m2 = Rectangle(6, 8, 11, 3, 1)
-        m2.update(**m1.to_dictionary())
-        self.assertNotEqual(m1, m2)
-
-    def test_to_dictionary_arg(self):
-        r = Rectangle(10, 20, 6, 1, 2)
-        with self.assertRaises(TypeError):
-            r.to_dictionary(1)
+            Square.display()
+        p = "display() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), p)
 
 if __name__ == "__main__":
     unittest.main()
