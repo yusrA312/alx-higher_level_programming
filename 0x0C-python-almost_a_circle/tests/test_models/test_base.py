ef0fb02f3a6cd0568c1049365943e4f0f6e3c0dc
yusrA312 <yusra2yahya@gmail.com>
2024-01-16 00:13:12 -0800

    done

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,14 +1,72 @@
 #!/usr/bin/python3
-"""Module for Base unit tests."""
+'''Module Base'''
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-
 class TestBase(unittest.TestCase):
+    def test_instantiation(self):
+
+        with self.assertRaises(ValueError) as e:
+            m = Square(-71)
+        msg = "width must be > 0"
+        self.assertEqual(str(e.exception), msg)
+
+        with self.assertRaises(ValueError) as e:
+            m = Square(16, -662)
+        msg = "x must be >= 0"
+        self.assertEqual(str(e.exception), msg)
+
+        with self.assertRaises(ValueError) as e:
+            m = Square(1, 23, -83)
+        msg = "y must be >= 0"
+        self.assertEqual(str(e.exception), msg)
+
+        with self.assertRaises(ValueError) as e:
+            m = Square(0)
+        msg = "width must be > 0"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_no_arg(self):
+        m = Base()
+        b = Base()
+        self.assertEqual(m.id, b.id - 1)
+
+    def test_three_bases(self):
+        m = Base()
+        n = Base()
+        r = Base()
+        self.assertEqual(m.id, n.id - 1)
+
+    def test_None_id(self):
+        m = Base(None)
+        b = Base(None)
+        self.assertEqual(m.id, b.id - 1)
+
+    def test_unique_id(self):
+        self.assertEqual(11, Base(11).id)
+
+    def test_nb_instances(self):
+        m = Base()
+        n = Base(11)
+        r = Base()
+        self.assertEqual(m.id, n.id - 8)
+
+    def test_class(self):
+        self.assertEqual(str(Square),
+                         "<class 'models.square.Square'>")
+
+    def test_inheritance(self):
+        self.assertTrue(issubclass(Square, Base))
+
+    def test_constructor(self):
+        with self.assertRaises(TypeError) as e:
+            m = Square()
+        n = "__init__() missing 1 required positional argument: 'size'"
+        self.assertEqual(str(e.exception), n)
+
     def test_from_json_string_l(self):
-        """Tests to_json_string() signature:"""
         with self.assertRaises(TypeError) as m:
             Base.from_json_string()
         n = "from_json_string() missing 1 required positional argument: \
@@ -20,25 +78,25 @@ class TestBase(unittest.TestCase):
 
         n = '[{"x": 4, "y": 4, "width": 4, "id": 4, "height": 4}, \
 {"x": 10, "y": 20, "width": 34, "id": 44, "height": 340}]'
-        k = [
-            {"x": 4, "y": 4, "width": 4, "id": 4, "height": 4},
-            {"x": 10, "y": 20, "width": 34, "id": 44, "height": 340},
-        ]
+        k = [{'x': 4, 'y': 4, 'width': 4, 'id': 4, 'height': 4},
+             {'x': 10, 'y': 20, 'width': 34, 'id': 44,
+             'height': 340}]
         self.assertEqual(Base.from_json_string(n), k)
 
+ 
     def test_from_json_string_t(self):
         list_in = [{"id": 80, "width": 20, "height": 74}]
         json_list_in = Rectangle.to_json_string(list_in)
         list_out = Rectangle.from_json_string(json_list_in)
         self.assertEqual(list, type(list_out))
 
-    def test_from_json_string_one_rectangle(self):
+    def test_from_json_string_o(self):
         list_in = [{"id": 80, "width": 20, "height": 74, "x": 97}]
         json_list_in = Rectangle.to_json_string(list_in)
         list_out = Rectangle.from_json_string(json_list_in)
         self.assertEqual(list_in, list_out)
 
-    def test_from_json_string_two_rectangles(self):
+    def test_from_json_string_two(self):
         list_in = [
             {"id": 80, "width": 20, "height": 74, "x": 97, "y": 88},
             {"id": 80, "width": 57, "height": 62, "x": 51, "y": 33},
@@ -51,19 +109,19 @@ class TestBase(unittest.TestCase):
         n = '[{"hi": 90}]'
         self.assertEqual(Base.from_json_string(n), m)
 
-        m = [{"x": 11, "y": 22, "width": 33, "id": 44, "height": 55}]
+        m = [{'x': 11, 'y': 22, 'width': 33, 'id': 44, 'height': 55}]
         n = '[{"x": 11, "y": 22, "width": 33, "id": 44, "height": 55}]'
         self.assertEqual(Base.from_json_string(n), m)
 
-        m = [{"x": 1, "y": 20, "width": 31, "id": 52, "height": 33}]
+        m = [{'x': 1, 'y': 20, 'width': 31, 'id': 52,
+             'height': 33}]
         n = '[{"x": 1, "y": 20, "width": 31, "id": 52, \
 "height": 33}]'
         self.assertEqual(Base.from_json_string(n), m)
 
     def test_o_save_to_file(self):
-        """Tests save_to_file() method."""
+        '''Tests save_to_file() method.'''
         import os
-
         m = Rectangle(10, 7, 6, 9)
         m1 = Rectangle(2, 5)
         Rectangle.save_to_file([m, m1])
@@ -82,3 +140,7 @@ class TestBase(unittest.TestCase):
         Rectangle.save_to_file([])
         with open("Rectangle.json", "r") as f:
             self.assertEqual(f.read(), "[]")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for Square unit tests."""
+'''ModuleSquare'''
 import unittest
 from models.base import Base
 from models.square import Square
@@ -9,17 +9,56 @@ import io
 
 
 class TestSquare(unittest.TestCase):
-    """Tests the Base class."""
+    '''Tests Base class.'''
 
-    def test_I_area_no_args(self):
-        m = Square(7)
+    def test_nan_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, float('nan'), 20)
+
+    def test_complex_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(7, 36, complex(45))
+
+    def test_dict_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(16, 10, {"a": 1, "b": 2})
+
+    def test_list_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(16, 1, [10, 20, 3])
+
+    def test_set_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 107, {16, 2, 73})
+
+    def test_tuple_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 1, (8, 2, 93))
+
+    def test_frozenset_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 3, frozenset({17, 26, 3, 1}))
+
+    def test_range_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 3, range(5))
+
+    def invalid_types(self):
+        n = (3.1, -1.51, float('inf'), float('-inf'), True, "hi", (4,),
+             [4], {4}, {9: 77}, None)
+        return n
+
+    def test_dict_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, {"a": 4, "b": 5}, 6)
+
+    def test_display(self):
+        m = Square(10)
         with self.assertRaises(TypeError) as e:
-            Square.area()
-        n = "area() missing 1 required positional argument: 'self'"
+            Square.display()
+        n = "display() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), n)
-    def test_list_size(self):
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square([8, 7, 6])
+
     def test_display_s(self):
         m = Square(1)
         n = io.StringIO()
@@ -75,6 +114,10 @@ class TestSquare(unittest.TestCase):
     def test_B_inheritance(self):
         self.assertTrue(issubclass(Square, Base))
 
+    def test_list_size(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square([8, 7, 6])
+
     def test_set_size(self):
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
             Square({7, 2, 7}, 9)
@@ -85,53 +128,6 @@ class TestSquare(unittest.TestCase):
             Square.display()
         p = "display() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), p)
-    def test_nan_x(self):
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            Square(1, float('nan'), 20)
 
-    def test_complex_y(self):
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(7, 36, complex(45))
-
-    def test_dict_y(self):
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(16, 10, {"a": 1, "b": 2})
-
-    def test_list_y(self):
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(16, 1, [10, 20, 3])
-
-    def test_set_y(self):
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(1, 107, {16, 2, 73})
-
-    def test_tuple_y(self):
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(1, 1, (8, 2, 93))
-
-    def test_frozenset_y(self):
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(1, 3, frozenset({17, 26, 3, 1}))
-
-    def test_range_y(self):
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(1, 3, range(5))
-
-    def invalid_types(self):
-        n = (3.1, -1.51, float('inf'), float('-inf'), True, "hi", (4,),
-             [4], {4}, {9: 77}, None)
-        return n
-
-    def test_dict_x(self):
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            Square(1, {"a": 4, "b": 5}, 6)
-
-    def test_display(self):
-        m = Square(10)
-        with self.assertRaises(TypeError) as e:
-            Square.display()
-        n = "display() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(e.exception), n)
 if __name__ == "__main__":
     unittest.main()
-			
