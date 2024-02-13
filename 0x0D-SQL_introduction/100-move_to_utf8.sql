cd0218f4d05c6226ca24729389c513dae2b24a01
yusrA312 <yusra2yahya@gmail.com>
2024-02-13 07:44:29 -0800

    done

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
---  'convert_to hbtn_0c_0 database to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
