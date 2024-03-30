f47d839298bb7a016a39cafb041c02690943711d
yusrA312 <yusra2yahya@gmail.com>
2024-03-30 04:55:30 -0700

    done

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+in a URL, sends a request and displays
+
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as reyy:
+        H = reyyinfo()
+        Yyget = H.get('X-Request-Id')
+        print(Yyget)
