7d4020796e48c08e1db67ba13f37a38d15689045
yusrA312 <yusra2yahya@gmail.com>
2024-03-30 17:36:12 -0700

    done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# takes in a letter
+from requests import post
+
+
+def search_user(query):
+    """Sends a request to the URL and displays the body of the response."""
+    URL = 'http://0.0.0.0:5000/search_user'
+    data = {'q': query}
+    response = post(URL, data)
+
+    type_res = response.headers['content-type']
+
+    if type_res == 'application/json':
+        result = response.json()
+        _id = result.get('id')
+        name = result.get('name')
+        if (result != {} and _id and name):
+            return "[{}] {}".format(_id, name)
+        else:
+            return 'No result'
+    else:
+        return 'Not a valid JSON'
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    query = argv[1] if len(argv) >= 2 else ""
+    print(search_user(query))
