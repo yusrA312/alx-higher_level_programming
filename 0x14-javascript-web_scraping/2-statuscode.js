f86cb8cf52f1eedd8deb2c7f73868171c516f08d
yusrA312 <yusra2yahya@gmail.com>
2024-04-23 04:14:55 -0700

    done

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Status Of Get request
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    process.stdout.write('code: ' + response.statusCode);
+  }
+});
