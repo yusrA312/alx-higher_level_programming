7c8310be89b8b7544d1105d2ff4f47a7a8cab4d4
yusrA312 <yusra2yahya@gmail.com>
2024-04-23 03:33:32 -0700

    done

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const files = require('fs');
+
+files.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+ });
