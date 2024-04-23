dd2d231b73e6f7186ee0e54185be9ad46ebdcb81
yusrA312 <yusra2yahya@gmail.com>
2024-04-23 03:13:57 -0700

    done

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const files = require('fs');
+
+files.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+    process.stdout.write(data);
+});
